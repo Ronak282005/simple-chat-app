0ac149f463d298ee3c387afd51de66672cc29561
ronak <ronakpraja340@gmail.com>
2025-06-22 18:10:30 +0530

    ip at BE

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -4,15 +4,19 @@ const wss = new WebSocketServer({ port: 8080 });
 
 const allSockets: WebSocket[] = [];
 
-wss.on("connection", (socket) => {
+wss.on("connection", (socket,req) => {
   console.log("====================================");
   console.log("connection made!");
   console.log("====================================");
+  const ip = req.socket.localAddress
+  console.log('====================================');
+  console.log(ip);
+  console.log('====================================');
   allSockets.push(socket);
   socket.on("message", (e) => {
     for (let i = 0; i < allSockets.length; i++) {
       const s = allSockets[i];
-      s.send(e.toString() + ": from the server");
+      s.send(e.toString() + ": from the " + ip);
     }
   });
 });
