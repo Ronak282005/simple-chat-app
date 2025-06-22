c9e32ab523beb0708a17b1b210677e2f6150ea10
ronak <ronakpraja340@gmail.com>
2025-06-22 13:23:52 +0530

    simple wss

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,3 +1,15 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port:8080})
+
+
+wss.on("connection",(socket)=>{
+    console.log('====================================');
+    console.log("connection made!");
+    console.log('====================================');
+    socket.onmessage = (ev)=>{
+        console.log('====================================');
+        console.log(ev.data);
+        console.log('====================================');
+    }
+})
