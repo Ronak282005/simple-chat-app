4e147b2ec1fc1a22b999058c148e105448ae6711
ronak <ronakpraja340@gmail.com>
2025-06-22 13:42:13 +0530

    simple wss chat room

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,15 +1,18 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({ port: 8080 });
 
+const allSockets: WebSocket[] = [];
 
-wss.on("connection",(socket)=>{
-    console.log('====================================');
-    console.log("connection made!");
-    console.log('====================================');
-    socket.onmessage = (ev)=>{
-        console.log('====================================');
-        console.log(ev.data);
-        console.log('====================================');
+wss.on("connection", (socket) => {
+  console.log("====================================");
+  console.log("connection made!");
+  console.log("====================================");
+  allSockets.push(socket);
+  socket.on("message", (e) => {
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      s.send(e.toString() + ": from the server");
     }
-})
+  });
+});
