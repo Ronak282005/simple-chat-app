c108d4c09f8ae9ee72bfff73262b7f6548363f91
ronak <ronakpraja340@gmail.com>
2025-06-22 22:45:13 +0530

    chat debugs

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,35 +1,44 @@
-import { WebSocket, WebSocketServer } from "ws";
-
-interface User {
-    socket : WebSocket;
-    room : string
-}
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const allSockets: User[] = []; 
+interface User {
+    socket: WebSocket;
+    room: string;
+}
 
-wss.on("connection", (socket,req) => {
-  console.log("====================================");
-  console.log("connection made!");
-  console.log("====================================");
-  socket.on("message", (e) => {
-      const parsedMessage = JSON.parse(e as unknown as string)
-      if(parsedMessage.type === "join"){
-        allSockets.push({
-            socket,
-            room : parsedMessage.payload.roomId
-        })
-      }
-      if(parsedMessage.type === "chat"){
-        const currentUser = allSockets.find((x)=>{
-            x.socket == socket
-        })?.room
+let allSockets: User[] = [];
 
-        const msg = allSockets.find((x)=>{
-            x.room == currentUser
-        })?.socket.send(parsedMessage.payload.message)
-      }
+wss.on("connection", (socket) => {
 
-  });
-});
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+
+        if (parsedMessage.type == "chat") {
+            console.log("user wants to chat");
+            // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+
+    })
+
+})
